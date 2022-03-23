686c6812ba02172e3027bb8cc6ca303c18246687
jinyi-x <jinyi.12@qq.com>
2022-03-23 22:33:57 +0800

    Add Configuration Header

--- Headers/MacroDefinition.hpp
+++ Headers/MacroDefinition.hpp
@@ -1,4 +1,13 @@
 #ifndef __MULTIMETER_MACRO_DEFINITION_HEADER__
 #define __MULTIMETER_MACRO_DEFINITION_HEADER__
 
+#define ADC_DRDY_PIN    12
+#define ADC_SC_PIN      13
+#define ADC_DOUT_PIN    14
+#define ADC_SCLK_PIN    15
+#define ADC_DIN_PIN     16
+
+#define MUX_A_PIN       4
+#define MUX_B_PIN       5
+
 #endif
