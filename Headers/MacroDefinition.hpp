25570971102d780008f967b3b0d1c27e6588bbb5
jinyi-x <jinyi.12@qq.com>
2022-03-26 01:34:04 +0800

    Change Pin Configuration

--- Headers/MacroDefinition.hpp
+++ Headers/MacroDefinition.hpp
@@ -6,8 +6,8 @@
 #define ADC_DRDY_PIN    GPIO_NUM_12
 #define ADC_CS_PIN      GPIO_NUM_13
 #define ADC_DOUT_PIN    GPIO_NUM_14
-#define ADC_SCLK_PIN    GPIO_NUM_15
-#define ADC_DIN_PIN     GPIO_NUM_16
+#define ADC_SCLK_PIN    GPIO_NUM_16
+#define ADC_DIN_PIN     GPIO_NUM_15
 
 #define MUX_A_PIN       GPIO_NUM_4
 #define MUX_B_PIN       GPIO_NUM_5
